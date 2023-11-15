cc8e83eae0286cf3fc6df43bab5f1b8b1b649fc1
ai82 <146689250+ai82@users.noreply.github.com>
2023-11-15 15:20:00 +0000

    [Finish] 3

--- bookers/app/controllers/books_controller.rb
+++ bookers/app/controllers/books_controller.rb
@@ -1,17 +1,43 @@
 class BooksController < ApplicationController
   def top
-    @lists = Book.all
+    @books = Book.all
+    @book = Book.new
   end
   def new
-    @list = List.new
+    @book = Book.new
   end
   def create
-    list = List.new(list_params)
-    list.save
-    redirect_to '/top'
+    @books = Book.all
+    @book = Book.new(list_params)
+    if @book.save
+      redirect_to book_path(@book.id)
+    else
+      render :top
+    end
+  end
+  def index
+    @books = Book.all
+  end
+
+  def show
+    @book = Book.find(params[:id])
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+  def update
+    book = Book.find(params[:id])
+    book.update(list_params)
+    redirect_to book_path(book.id)
+  end
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to '/books'
   end
   private
-  def list_param
-    params.require(:list).pemit(:title, :body)
+  def list_params
+    params.require(:book).permit(:title, :body)
   end
 end
--- bookers/config/routes.rb
+++ bookers/config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
-  get 'lists/new'
-  get 'lists/index'
-  get 'lists/edit'
+  get 'books/new'
+  get 'books/index'
   get '/' => 'homes#top'
-  get 'books/' => 'books#top'
-  post 'books/' => 'lists#create'
-  get 'books/lists' => 'books#top'
-  get 'books/:id' => 'lists#show'
+  get 'books' => 'books#top'
+  post 'books' => 'books#create'
+  get 'books/:id' => 'books#show', as: 'book'
+  get 'books/:id/edit' => 'books#edit', as: 'edit_book'
+  patch 'books/:id' => 'books#update', as: 'update_book'
+  delete 'books/:id' => 'books#destroy', as: 'destroy_book'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
