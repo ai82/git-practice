036d4f5f5a9c1105322e025d4a3e2650082aa3bd
ai82 <146689250+ai82@users.noreply.github.com>
2023-11-19 14:29:31 +0000

    [Finish2] 3

--- bookers/app/controllers/books_controller.rb
+++ bookers/app/controllers/books_controller.rb
@@ -1,23 +1,17 @@
 class BooksController < ApplicationController
-  $update_check_flag
-  def top
-    @books = Book.all
-    @book = Book.new
-  end
-  def new
-    @book = Book.new
-  end
   def create
-    @books = Book.all
-    @book = Book.new(list_params)
+    @book = Book.new(book_params)
     if @book.save
+      flash[:notice] = "Book was successfully created."
       redirect_to book_path(@book.id)
     else
-      render :top
+      @books = Book.all
+      render :index
     end
   end
   def index
     @books = Book.all
+    @book = Book.new
   end
 
   def show
@@ -29,8 +23,8 @@ class BooksController < ApplicationController
   end
   def update
     @book = Book.find(params[:id])
-    if @book.update(list_params)
-      $update_check_flag = true
+    if @book.update(book_params)
+      flash[:notice] = "Book was successfully updated."
       redirect_to book_path(@book.id)
     else
       render :edit
@@ -39,10 +33,11 @@ class BooksController < ApplicationController
   def destroy
     book = Book.find(params[:id])
     book.destroy
+    flash[:notice] = "Book was successfully destroyed."
     redirect_to '/books'
   end
   private
-  def list_params
+  def book_params
     params.require(:book).permit(:title, :body)
   end
 end
